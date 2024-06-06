2f87fba1bf1a7178cf35131fa0e490415a3f0b8b
Yeji <mnbvcxzyj@gmail.com>
2024-06-06 16:59:20 +0900

    first commit

---
+++ src/main/java/com/wcsp/web_code_security_project/WebCodeSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wcsp.web_code_security_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCodeSecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCodeSecurityProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wcsp/web_code_security_project/WebCodeSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcsp.web_code_security_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCodeSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
