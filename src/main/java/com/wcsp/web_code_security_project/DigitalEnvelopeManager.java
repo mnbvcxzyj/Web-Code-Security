e7dde0b6d27dc6c859069afb032c41c7558dc596
Yeji <mnbvcxzyj@gmail.com>
2024-06-09 20:15:17 +0900

    Apply .gitignore

--- src/main/java/com/wcsp/web_code_security_project/DigitalEnvelopeManager.java
+++ src/main/java/com/wcsp/web_code_security_project/DigitalEnvelopeManager.java
@@ -41,6 +41,12 @@ public class DigitalEnvelopeManager {
     // ? : 1) (전자서명, 원문, 자신의 공개키)를 자기 비밀키로 암호화 / 2) 전자봉투 = 영희 공개키로 시크릿키를 암호화
     // ? : 1) 비밀키로 암호화한 결과 + 2) 비밀키가 암호화된 전자봉투를 보냄
     public DigitalEnvelope createEnvelope(String data, PublicKey publicKey, PrivateKey privateKey) throws GeneralSecurityException, NoSuchAlgorithmException {
+
+        // #4) 보안에 민감한 메소드들이 검증된 매개변수를 가지고 호출되도록 보장하라
+        if (data == null || data.isEmpty() || publicKey == null || privateKey == null) {
+            throw new IllegalArgumentException("검증 되지 않은 매개변수 입니다.");
+        }
+
         // 객체 생성
         Signature sig = Signature.getInstance(SIGN_ALGORITHM);
 
